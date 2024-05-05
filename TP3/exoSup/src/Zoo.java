921e7d15fc67d06c32fe1ebfa2dc701ed17ec261
Danil gdj <danilguidjou7@gmail.com>
2024-05-05 19:25:56 +0100

    TP3

--- TP3/exoSup/src/Zoo.java
+++ TP3/exoSup/src/Zoo.java
@@ -1,12 +1,11 @@
 public class Zoo {
-    private Animal[] tabAnimals;
-
-    public Zoo() {
-        this.tabAnimals[1] = new Mammifere("vache", 5);
-        this.tabAnimals[2] = new Mammifere("chien", 3);
-        this.tabAnimals[3] = new Oiseau("Meqnin", "dehors");
-        this.tabAnimals[4] = new Oiseau("canari", "cage");
-        this.tabAnimals[5] = new Oiseau("hiboux", "arbre");
+    public static void main(String[] args) throws Exception {
+        Animal[] tabAnimals = new Animal[5];
+        tabAnimals[0] = new Mammifere("vache", 5);
+        tabAnimals[1] = new Mammifere("chien", 3);
+        tabAnimals[2] = new Oiseau("Meqnin", "dehors");
+        tabAnimals[3] = new Oiseau("canari", "cage");
+        tabAnimals[4] = new Oiseau("hiboux", "arbre");
         for (int i = 0; i < tabAnimals.length; i++) {
             tabAnimals[i].manger();
             tabAnimals[i].dormir();
