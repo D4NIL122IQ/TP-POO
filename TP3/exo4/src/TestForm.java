f06d4df9757cb1e93291889c1f3d80d68f17a89e
Danil gdj <danilguidjou7@gmail.com>
2024-05-05 19:51:03 +0100

    TP3

--- TP3/exo4/src/TestForm.java
+++ TP3/exo4/src/TestForm.java
@@ -3,5 +3,6 @@ public class TestForm {
     public TestForm() {
         Cercle oc = new Cercle(5.2);
         System.out.println("le perimetre du cercle est : " + oc.perimetre());
+        System.out.println("la surface du cercle est : " + oc.surface());
     }
 }
